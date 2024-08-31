b264f98116ea8e3fe9202554660929f8031a13af
Jeroen van Warmerdam <jeronevw@hotmail.com>
2024-08-31 17:06:22 +0200

    Suppress CA1869 in tests

---
+++ Jvw.DevToys.SemverCalculator.Tests/Assembly.cs
@@ -0,0 +1,3 @@
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1869", Justification = "Do not cache JsonSerializerOptions in tests.")]
--- Jvw.DevToys.SemverCalculator.Tests/DictionaryToKeysListConverterTests.cs
+++ Jvw.DevToys.SemverCalculator.Tests/DictionaryToKeysListConverterTests.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using System.Text.Json;
 using Jvw.DevToys.SemverCalculator.Converters;
 
