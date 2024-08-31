a23aed0eeaee9c73ac381c80ba89ae07376d0b7e
Jeroen van Warmerdam <jeronevw@hotmail.com>
2024-08-31 20:29:35 +0200

    CSharpier test proj

--- Jvw.DevToys.SemverCalculator.Tests/Assembly.cs
+++ Jvw.DevToys.SemverCalculator.Tests/Assembly.cs
@@ -1,3 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Performance", "CA1869", Justification = "Do not cache JsonSerializerOptions in tests.")]
+[assembly: SuppressMessage(
+    "Performance",
+    "CA1869",
+    Justification = "Do not cache JsonSerializerOptions in tests."
+)]
