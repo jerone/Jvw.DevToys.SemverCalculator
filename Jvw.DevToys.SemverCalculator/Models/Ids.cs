d9bac7eeb56596d1b1e8e8f623656b7952cd344f
Jeroen van Warmerdam <jeronevw@hotmail.com>
2024-10-10 20:42:36 +0200

    Add package manager setting

--- Jvw.DevToys.SemverCalculator/Models/Ids.cs
+++ Jvw.DevToys.SemverCalculator/Models/Ids.cs
@@ -5,13 +5,14 @@ namespace Jvw.DevToys.SemverCalculator.Models;
 /// </summary>
 internal static class Ids
 {
+    internal const string HttpAgreementInfoBar = "HttpAgreementInfoBar";
+    internal const string PackageManagerSetting = "PackageManagerSetting";
     internal const string PackageNameInput = "PackageNameInput";
     internal const string PackageNameWarningBar = "PackageNameWarningBar";
     internal const string VersionRangeInput = "VersionRangeInput";
     internal const string VersionRangeWarningBar = "VersionRangeWarningBar";
     internal const string VersionsList = "VersionsList";
     internal const string ProgressRing = "ProgressRing";
-    internal const string HttpAgreementInfoBar = "HttpAgreementInfoBar";
     internal const string PreReleaseToggle = "PreReleaseToggle";
     internal const string PackageLoadButton = "PackageLoadButton";
 }
